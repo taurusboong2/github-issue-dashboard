a466763d1d24ac9a778462bc881e030538ab5884
boong <taurusboong2@gmail.com>
2023-01-14 17:46:44 +0900

    feat: localStorage functions

---
+++ src/commons/localStorage.ts
@@ -0,0 +1,24 @@
+export const getItem = (key: string): string | null => {
+  let rawValue;
+  if (typeof window !== 'undefined') {
+    rawValue = localStorage.getItem(key);
+  }
+  if (!rawValue) return null;
+  try {
+    return rawValue;
+  } catch (error) {
+    return null;
+  }
+};
+
+export const setItem = (key: string, value: any) => {
+  localStorage.setItem(key, value);
+};
+
+export const removeItem = (key: string) => {
+  localStorage.removeItem(key);
+};
+
+export const clear = () => {
+  localStorage.clear();
+};

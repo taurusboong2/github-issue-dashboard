ac6e67cb4503ba0ea50686cad79adcf79ce55834
boong <taurusboong2@gmail.com>
2023-01-14 19:04:32 +0900

    feat: routing issues page

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,20 @@ import { Route, Routes } from 'react-router-dom';
 import { hot } from 'react-hot-loader/root';
 
 import GlobalStyle from '@/styles/globalStyles';
+import { Nav } from '@/components/common';
 import Home from '@/pages/home';
+import Issues from '@/pages/issues';
 
 const App: React.FC = () => {
   return (
     <>
       <GlobalStyle />
 
+      <Nav />
+
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/issues" element={<Issues />} />
       </Routes>
     </>
   );

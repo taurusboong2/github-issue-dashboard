27cbb219d9f5bbec8adfaac4aa7f82fe9db4f986
boong <taurusboong2@gmail.com>
2023-01-15 19:47:37 +0900

    feat: click logo to go home

--- src/components/common/Nav/Nav.tsx
+++ src/components/common/Nav/Nav.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router';
 
 import { Box } from '@/components/common';
 import { IconGithub } from '@/components/icons';
 
 const Nav = () => {
+  const navigate = useNavigate();
+
   return (
     <Container>
       <Box height="100%" p="0 30px" flexDirection="row" justifyContent="space-between" alignItems="center">
-        <Box flexDirection="row" alignItems="center" justifyContent="center">
+        <NavLogoBox onClick={() => navigate('/')}>
           <IconGithub />
           <Title>My Repository Issue</Title>
-        </Box>
+        </NavLogoBox>
       </Box>
     </Container>
   );
@@ -27,6 +30,13 @@ const Title = styled.h1`
   margin-left: 5px;
   font-size: 20px;
   font-weight: bold;
+`;
+
+const NavLogoBox = styled(Box)`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
 `;
 
 export default Nav;
